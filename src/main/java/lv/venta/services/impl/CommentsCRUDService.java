9f194c9bd35ed8b4382616d3b13e941d9163c99b
Arvids Ivbuls <arvidsivbuls@gmail.com>
2023-06-29 23:51:12 +0300

    bugfix Renars

--- src/main/java/lv/venta/services/impl/CommentsCRUDService.java
+++ src/main/java/lv/venta/services/impl/CommentsCRUDService.java
@@ -2,11 +2,14 @@ package lv.venta.services.impl;
 
 
 import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lv.venta.models.Comments;
 import lv.venta.models.users.Person;
+import lv.venta.repos.IRepoComments;
 import lv.venta.services.ICommentsCRUDService;
 
 @Service

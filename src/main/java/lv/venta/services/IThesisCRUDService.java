eb08c861e2996b6aa67b65dee866490f1ce6027d
reinisrm <rreeiinniiss@gmail.com>
2023-06-29 23:10:28 +0300

    typo

--- src/main/java/lv/venta/services/IThesisCRUDService.java
+++ src/main/java/lv/venta/services/IThesisCRUDService.java
@@ -1,18 +1,21 @@
 package lv.venta.services;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import lv.venta.models.Thesis;
 
 public interface IThesisCRUDService {
+    
+    public List<Thesis> getAll();
 
-	void insertNewThesis(Thesis thesis);
-	
-	Thesis selectThesisById(long id) throws Exception;
-	
-	void deleteThesis(long id) throws Exception;
-	
-	void updateThesis(Thesis updatedThesis) throws Exception;
-	
-	ArrayList<Thesis> selectAllThesis();
+    void insertNewThesis(Thesis thesis);
+    
+    Thesis selectThesisById(long id) throws Exception;
+    
+    void deleteThesis(long id) throws Exception;
+    
+    void updateThesis(Thesis updatedThesis) throws Exception;
+    
+    ArrayList<Thesis> selectAllThesis();
 }
